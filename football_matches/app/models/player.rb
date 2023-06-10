f3a8b63b63d469b7f67a6f4e6461a420e1ae2f3b
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-10 03:50:37 +0300

    simplify

--- football_matches/app/models/player.rb
+++ football_matches/app/models/player.rb
@@ -1,16 +1,14 @@
 class Player < ApplicationRecord
   belongs_to :team
   has_many :matches, Matches.lambda
+  has_many :player_metrics
 
   def self.top_five_by(factor)
-    ids = Match.unscope(:order).select(:player_id)
-               .joins(:player_metrics)
-               .where("factors -> ? = 'true'", factor)
-               .group(:player_id)
-               .order(:player_id)
-               .limit(5)
-               .count.keys
-
-    Player.where(id: ids)
+    Player.select('players.*')
+          .joins(:player_metrics)
+          .where("factors -> ? = 'true'", factor)
+          .group(:id)
+          .order(:id)
+          .limit(5)
   end
 end
