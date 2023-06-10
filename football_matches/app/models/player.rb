72518f8356b971462da1b9a86880ee51550ff13a
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-10 04:00:56 +0300

    top five by team

--- football_matches/app/models/player.rb
+++ football_matches/app/models/player.rb
@@ -3,10 +3,11 @@ class Player < ApplicationRecord
   has_many :matches, Matches.lambda
   has_many :player_metrics
 
-  def self.top_five_by(factor)
+  def self.top_five_by(factor, team_id = nil)
     Player.select('players.*')
           .joins(:player_metrics)
           .where("factors -> ? = 'true'", factor)
+          .where(team_id ? 'team_id = ?' : 'true', team_id)
           .group(:id)
           .order(:id)
           .limit(5)
--- football_matches/app/models/team.rb
+++ football_matches/app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
   has_many :players
   has_many :matches, Matches.lambda
+
+  def top_five_players_by(factor)
+    Player.top_five_by(factor, id)
+  end
 end
